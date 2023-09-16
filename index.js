88a202843b5e5cae166f0dcf3560f4252ea42bd7
Ritesh Yadav <88195857+Riteshy123@users.noreply.github.com>
2023-09-16 23:08:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const path=require('path');
-const     PORT =process.env.PORT || 8000;
+const  PORT = 8000;
 const app=express();
 app.set('view engine','ejs') ;
 app.set('views',path.join(__dirname,'view'));
